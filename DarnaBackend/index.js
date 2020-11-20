a1f43b71554c76f5e4ff77d51c79ad98c22d6fbd
ESLEMM <islemkhemiri09@gmail.com>
2020-11-20 21:56:58 +0100

    organiser

--- DarnaBackend/index.js
+++ DarnaBackend/index.js
@@ -7,7 +7,7 @@ import routes_image from './src/Routes/image_routes';
 
 const app = express();
 const cors = require('cors');
-const PORT =8080 ;
+const PORT = 8080;
 var serveStatic = require('serve-static');
 var path = require('path');
 const morgan = require('morgan');
@@ -21,9 +21,9 @@ const fs = require("fs");
 
 //connection mongoose :DB
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/DarnaDB',{
-    useUnifiedTopology: true,
-    useNewUrlParser: true
+mongoose.connect('mongodb://localhost/DarnaDB', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
 }).then(() => {
   console.log('Database sucessfully connected')
 },
@@ -33,10 +33,10 @@ mongoose.connect('mongodb://localhost/DarnaDB',{
 )
 app.use(serveStatic(path.join(__dirname, 'Darna'), {
   maxAge: '1d',
- // setHeaders: setCustomCacheControl
+  // setHeaders: setCustomCacheControl
 }));
 //bodyparser :midlleware pour passer les informations à la base de données
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 var corsOptions = {
@@ -44,26 +44,26 @@ var corsOptions = {
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
 }
 app.use(cors(corsOptions));
-app.use( (req, res, next) => {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
   next();
-});  
+});
 
 routes_demand(app)
 routes_member(app)
 routes_image(app)
-  //app.use(express.static(__dirname + '../../views'));
-  global.__basedir = __dirname;
- 
+//app.use(express.static(__dirname + '../../views'));
+global.__basedir = __dirname;
+
 //Port
 app.get('/', (req, res) =>
-res.send(`serveur node et express sur port ${PORT} `)
+  res.send(`serveur node et express sur port ${PORT} `)
 );
 
 app.listen(PORT, () =>
-console.log(`votre serveur est sur le port ${PORT} `)
+  console.log(`votre serveur est sur le port ${PORT} `)
 );
 
 module.exports = app
