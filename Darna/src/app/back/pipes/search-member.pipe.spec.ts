e7f7c386341ac2fce2af1d5fdc1f11d61a6eb0da
ESLEMM <islemkhemiri09@gmail.com>
2020-11-28 23:57:18 +0100

    28112020 2357

--- Darna/src/app/back/pipes/search-member.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SearchMemberPipe } from './search-member.pipe';
-
-describe('SearchMemberPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SearchMemberPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
